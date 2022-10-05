44268ddb93d6644a218df275fa237ab0b813d732
Breno Xavier <brenoxavier1227@gmail.com>
2022-10-05 08:27:33 -0300

    Adicionando .dockerignore

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,9 @@
 <?php
 
-use Carbon\Carbon;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return [
-        'timestamp' => Carbon::now()
+        'ping' => 'pong'
     ];
 });
